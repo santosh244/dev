1ef23ea60a0334cdb6ac890ea3b01efedc1614e2
Santosh <skskumar448@gmail.com>
2018-12-26 08:19:22 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = new express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('capitalize', (text) => {
+  return text.toUpperCase();
+});
+
+app.use((req, res, next) => {
+  var now  = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n' , (err) => {
+    if(err){
+      console.log('Unable to write log');
+    }
+  });
+
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render('maintenance.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.render('home.hbs',{
+      welcomeMessage: 'Hello bruh !',
+      pageTitle:'About',
+  //    currentYear: new Date().getFullyear()
+    });
+});
+
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle:'About',
+    // currentYear: new Date().getFullyear()
+  });
+});
+
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Errrrr!... server crashed'
+  })
+});
+
+app.listen(3000);
